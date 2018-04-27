86f9663be7c5b378dac34f711116829e9e272308
Mr.Moai <mr_moai_2016@hotmail.com>
2018-04-27 17:28:53 +0900

    Release Ver2.0

--- src/moai/moai_for_android.sh
+++ src/moai/moai_for_android.sh
@@ -1,9 +1,6 @@
 #!/system/bin/sh
 
 chmod 755 ./moai
-chmod 755 ./cgis/easter/*.cgi
-chmod 755 ./cgis/custom_boy/*.cgi
-chmod 755 ./cgis/cgi_samples/*.cgi
-chmod 755 ./cgis/proxy_finder/*.cgi
+find ./cgis/ -name \*.cgi -exec chmod 755 {} \;
 env LD_LIBRARY_PATH=$LD_LIBRARY_PATH:. ./moai
 
